0cdddca8e0ba0e50b24cebbbeef06615d9434d18
Ben Gordon <brgordon@ua.edu>
2014-04-23 13:21:44 -0500

    Version checking now works this time.

--- lib/longbow/version.rb
+++ lib/longbow/version.rb
@@ -2,11 +2,10 @@ $:.push File.expand_path('../', __FILE__)
 require 'colors'
 
 module Longbow
-  VERSION = '0.1.2'
+  VERSION = '0.1.3'
 
   def self.check_for_newer_version
-    v = Gem.latest_version_for 'longbow'
-    unless v == VERSION
+    unless Gem.latest_version_for('longbow').to_s == VERSION
       Longbow::purple "\n  A newer version of longbow is available. Run '[sudo] gem update longbow'."
     end
   end
